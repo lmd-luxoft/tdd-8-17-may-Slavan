03dbb2e81b0724f4e29d0377668fcb639a1097b3
Viacheslav Alexeyenko <vvalexeyenko@gmail.com>
2023-05-10 10:32:21 +0200

    solution fix

--- src/TDDKata.Lib/Calc.cs
+++ src/TDDKata.Lib/Calc.cs
@@ -10,13 +10,19 @@
             expression = expression.Replace("  ", " ");
             var args = expression.Trim().Split(' ');
             if (args.Length > 2)
-                throw new ArgumentException(" To much inputs");
+            {
+                //throw new ArgumentException(" To much inputs");
+                return -1;
+            }
 
             var values = new int[args.Length];
 
             for (int i = 0; i < args.Length; i++)
                 if (!int.TryParse(args[i], out values[i]))
-                    throw new ArgumentException($"{i} argument '{args[i]}' is not valid int");
+                {
+                    //throw new ArgumentException(" To much inputs");
+                    return -1;
+                }
 
             if (values.Length == 1)
                 return values[0];
