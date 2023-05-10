71f4e9c4eadbfb301724937817e79013306a9158
Viacheslav Alexeyenko <vvalexeyenko@gmail.com>
2023-05-10 10:02:34 +0200

    solution

--- src/TDDKata.Lib/Calc.cs
+++ src/TDDKata.Lib/Calc.cs
@@ -4,7 +4,24 @@
     {
         public int Add(string expression)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(expression))
+                return 0;
+
+            expression = expression.Replace("  ", " ");
+            var args = expression.Trim().Split(' ');
+            if (args.Length > 2)
+                throw new ArgumentException(" To much inputs");
+
+            var values = new int[args.Length];
+
+            for (int i = 0; i < args.Length; i++)
+                if (!int.TryParse(args[i], out values[i]))
+                    throw new ArgumentException($"{i} argument '{args[i]}' is not valid int");
+
+            if (values.Length == 1)
+                return values[0];
+
+            return values[0] + values[1];
         }
     }
 }
